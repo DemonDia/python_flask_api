624f693dd29130ad8a6c4eb2b900b8fdc9ba0a5e
Setsuna Yukiii <siang-meng@live.com>
2022-09-13 21:06:09 +0800

    chore: adding an empty route (confirm API is working)

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from Routes.VideoRoutes import *
 # =================User API routes=================
 from Routes.UserRoutes import *
 
+@app.route("/")
+def hello():
+    return "hello"
 # =================run the app=================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

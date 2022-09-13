1a642d3c489ffb3a7d0b146f45305f32f2810f72
Setsuna Yukiii <siang-meng@live.com>
2022-09-13 22:13:19 +0800

    Revert "changing of port"
    This reverts commit fe01486e58cbe153cd6d9a606a9e37eba7586ef9.

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def hello():
     return "hello"
 # =================run the app=================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8501)
+    app.run(host="0.0.0.0", port=5000)
